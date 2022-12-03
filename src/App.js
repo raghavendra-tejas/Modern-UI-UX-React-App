8a309b8d763d7fc174f3a162e3e2e66eee67ee5b
raghavendra-tejas <tejasraghavendra8@gmail.com>
2022-12-03 20:02:40 +0530

    App necessary folders and file structures created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import React from "react";
+
+import {
+  Blog,
+  Footer,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers/export";
+
+import { Cta, Brand, Navbar } from "./components/export";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
